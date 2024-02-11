5cf86cb090101e8a93e246be714073a4ab0c5d19
Michael Muniu <muniu.dev@gmail.com>
2024-02-11 11:18:27 +0300

    Lab Complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    
+def display_dwarves(names_list):
+    for index, name in enumerate(names_list):
+        print(f"{index + 1}. {name}")
 
-def summon_captain_planet():
-    
+def summon_captain_planet(elements_list):
+    return [element.capitalize() + "!" for element in elements_list]
 
-def long_planeteer_calls():
-    pass
+def has_long_call(calls_list):
+    for call in calls_list:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_cheese(strings_list):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for word in strings_list:
+        if word in cheeses:
+            return word
+    return None
